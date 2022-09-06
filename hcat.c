9450dfca9fabe9fb3808fa31424471935722bb09
manguuu <mingyuu.dev@gmail.com>
2022-09-06 08:36:30 +0000

    init

---
+++ hcat.c
@@ -0,0 +1,53 @@
+#include "types.h"
+#include "stat.h"
+#include "user.h"
+
+char buf[512];
+int printline;
+
+void 
+cat(int fd)
+{
+  int n;
+
+  int cnt = 0;
+  while((n = read(fd, buf, sizeof(buf))) > 0) {
+    for (int i = 0; i < n; i++) {
+        if (buf[i] == '\n') cnt++;
+        if (cnt == printline) {
+            n = i + 1;
+            break;
+        }
+    }
+    if (write(1, buf, n) != n) {
+      printf(1, "cat: write error\n");
+      exit();
+    }
+    if (cnt == printline) return;
+  }
+  if(n < 0){
+    printf(1, "cat: read error\n");
+    exit();
+  }
+}
+
+int
+main(int argc, char *argv[])
+{
+  int fd, i;
+  printline = atoi(argv[1]);
+  if (argc <= 2){
+    cat(0);
+    exit();
+  }
+
+  for(i = 2; i < argc; i++){
+    if((fd = open(argv[i], 0)) < 0){
+      printf(1, "cat: cannot open %s\n", argv[i]);
+      exit();
+    }
+    cat(fd);
+    close(fd);
+  }
+  exit();
+}
---
+++ ssu_login.c
@@ -0,0 +1,62 @@
+#include "types.h"
+#include "stat.h"
+#include "user.h"
+#include "fcntl.h"
+
+char userID[16][32];
+char pwdID[16][32];
+int user_num;
+
+int parse(int fd, char *s) {
+    char c;
+    for (int i = 0; i < 32; i++) {
+        if (read(fd, &c, 1) <= 0) return 0;
+        if (c == ' ' || c == '\n') return 1;
+        s[i] = c;
+    }
+    return 1;
+}
+
+void get_user_list() {
+    int fd = open("list.txt", O_RDONLY);
+    if (fd < 0) printf(1, "open list.txt error");
+    for (int i = 0; i < 16; i++, user_num++) {
+        if (!parse(fd, userID[i])) break;
+        if (!parse(fd, pwdID[i])) break;
+    }
+    close(fd);
+}
+
+int check_idpw(char *name, char *pwd) {
+    for (int i = 0; i < user_num; i++) {
+        if (!strcmp(name, userID[i]) && !strcmp(pwd, pwdID[i])) return 1;
+    }
+    return 0;
+}
+
+int main(int argc, char *argv[]) {
+    get_user_list();
+    char name[32];
+    char pwd[32];
+    printf(1, "Username: ");
+    parse(0, name);
+    printf(1, "Password: ");
+    parse(0, pwd);
+    if (!check_idpw(name, pwd)) {
+        printf(1, "login fail\n");
+        return 0;
+    }
+    int pid = fork();
+    if (pid < 0) {
+      printf(1, "init: fork failed\n");
+      exit();
+    }
+    if (pid == 0) {
+      exec("sh", argv);
+      printf(1, "init: exec sh failed\n");
+      exit();
+    }
+    int wpid;
+    while((wpid = wait()) >= 0 && wpid != pid) 
+        printf(1, "zombie!\n");
+}
